460ef1336ee017b88f1840db1573f7a508252fe2
Ohto-Ai <zhu.thatboy@outlook.com>
2023-04-26 02:57:22 +0800

    Add some args

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,38 @@ int main(int argc, char **argv)
 
     program.add_description(VersionHelper::getInstance().Description);
     program.add_epilog(fmt::format("Author: {} <{}>\nSite: {}", VersionHelper::getInstance().Author, VersionHelper::getInstance().Email, VersionHelper::getInstance().Homepage));
-    program.add_argument("-R", "--robot")
-            .help("Specify the robot")
-            .action([](const std::string &value) { std::cout << "Robot name: " << value << std::endl; });
+
+    argparse::ArgumentParser send_command("send", "", argparse::default_arguments::help);
+    send_command.add_description("Send message to robot");
+    send_command.add_argument("robot")
+                .help("Specify the robot");
+    send_command.add_argument("message")
+                .help("Specify the message");
+
+    argparse::ArgumentParser robot_command("robot", "", argparse::default_arguments::help);
+    robot_command.add_description("Manage robot");
+    robot_command.add_argument("list")
+                .help("List all robots");
+    robot_command.add_argument("add")
+                .help("Add a robot");
+    robot_command.add_argument("remove")
+                .help("Remove a robot");
+    robot_command.add_argument("update")
+                .help("Update a robot");
+
+    argparse::ArgumentParser execute_command("exec", "", argparse::default_arguments::help);
+    execute_command.add_description("Execute a command");
+    execute_command.add_argument("-R", "--robot")
+                .help("Specify the robot")
+                .action([](const std::string &value) { std::cout << "Robot name: " << value << std::endl; });
+    execute_command.add_argument("command")
+                .help("Specify the command")
+                .nargs(argparse::nargs_pattern::at_least_one)
+                .remaining();
+
+    program.add_subparser(send_command);
+    program.add_subparser(robot_command);
+    program.add_subparser(execute_command);
 
     try
     {
@@ -32,7 +61,7 @@ int main(int argc, char **argv)
     }
     catch (const std::exception &e)
     {
-        fmt::print(stderr, "{}\n", e.what());
+        fmt::print(stderr, "Exception occur when call `parse_args` ({})\n", e.what());
         std::exit(1);
     }
 
